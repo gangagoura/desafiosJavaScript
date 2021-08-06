f9d50312d0f042603aa16e44f02d1eef4af5f3bd
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2021-08-06 20:22:34 -0300

    Create OrdenandoNumerosParesEimpares.js

---
+++ OrdenandoNumerosParesEimpares.js
@@ -0,0 +1,15 @@
+const linhas = parseInt(gets());
+let entrada = [];
+
+for (let i = 0; i < linhas; i++) {
+  entrada = [...entrada, gets()];
+}
+
+entrada = entrada.sort((a, b) => a - b);
+const pares = entrada
+  .filter(n => n % 2 === 0);
+const impares = entrada
+  .filter(n => n % 2 === 1)
+  .reverse();
+const saida = [...pares, ...impares];
+saida.map((n) => console.log(n));
